f3933ed5fc420e7698c4fcf3cfc819d6c096e396
Usama Sultan <55051475+Usama-S@users.noreply.github.com>
2020-06-28 04:56:51 -0700

    Add files via upload

---
+++ Credit Card integration/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            net.authorize.sample.ChargeCreditCard.Run("2Rj27hrPz9c", "774ZJ99QjyG5G7nm", 1100);
+        }
+    }
+}
